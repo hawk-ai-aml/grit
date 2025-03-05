a8a50267f4969538ce78e10aab494586e2835625
Thinh Tran <thinh.tran@hawk.ai>
2025-03-05 12:48:37 +0100

    refactored to include typing in output.py

--- src/grit/output.py
+++ src/grit/output.py
@@ -31,11 +31,52 @@ class GritOut(ABC):
         self.services = services
         self.folder_name = folder_name
 
-        self.obj = []
+        self.dash_obj:Dashboard = []
+        self.alert_obj:AlertFileBasedProvisioning = []
 
     def init__alerts(self) -> None:
         """Optional: Initialize alerts for the dashboard."""
         print("Default init__alerts method. Override if needed.")
+
+    def init_dashboard(self, datasource, templating=[]) -> None:
+        """Initialize the GritDash Object and adds it to the processing list
+
+        Args:
+            datasource (Any): Datasource class object
+        """
+        from grit import GritDash
+
+        self.dash_obj.append(
+            GritDash(
+                uid=self.DASHBOARD_UUID,
+                title=self.DASHBOARD_TITLE,
+                dataSource=datasource,
+                description="This panel has been provisioned. Manual changes on the panel will be overridden if they are not persisted.",
+                tags=['hawk'],
+                stack=self.stack()
+            )
+        )
+
+    def init_alert_group(self, evaluateInterval:str) -> None:
+        """Initialize the GritAlert Object and adds it to the processing list
+
+        Args:
+            datasource (Any): Datasource class object
+        """
+        from grit import GritAlert, AlertRuleBuilder
+        from grafanalib.core import AlertGroup
+
+        self.alert_obj.append(
+            GritAlert(
+                uid="{}-alerts".format(self.DASHBOARD_UUID),
+                groups=[AlertGroup(
+                    name=self.DASHBOARD_TITLE,
+                    rules=AlertRuleBuilder.build_all(self.es_alerts),
+                    folder=self.folder_name,
+                    evaluateInterval=evaluateInterval
+                )]
+            )
+        )
 
     @abstractmethod
     def stack(self) -> Any:
@@ -56,12 +97,13 @@ class GritOut(ABC):
         os.makedirs(f"{out_base_dir}/{self.folder_name}/", exist_ok=True)
         os.makedirs(f"{out_base_dir_alerts}", exist_ok=True)
 
-        for _obj in self.obj:
+        for _obj in self.dash_obj:
             if isinstance(_obj, Dashboard):
                 with open(f"{out_base_dir}/{self.folder_name}/{self.DASHBOARD_UUID}.json", "w") as file:
                     file.write(json.dumps(
                         _obj.to_json_data(), sort_keys=True, indent=2, cls=DashboardEncoder))
 
+        for _obj in self.alert_obj:
             if isinstance(_obj, AlertFileBasedProvisioning):
                 with open(f"{out_base_dir_alerts}/{_obj.uid}.json", "w") as file:
                     file.write(json.dumps(
