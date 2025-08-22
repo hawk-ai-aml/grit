c50da3da497cd48733ece3315ab80fd907a12fd3
Thinh Tran <thinh.tran@hawk.ai>
2025-08-22 23:53:51 +0200

    Changed default value for errorstate to ALERTRULE_STATE_DATA_KEEPLAST_V11

--- src/grit/alert_rules_builder.py
+++ src/grit/alert_rules_builder.py
@@ -5,7 +5,7 @@ from .utilities import create_uid_from_string
 from grafanalib.core import (
     AlertExpression,
     EXP_TYPE_REDUCE, EXP_TYPE_MATH, EXP_REDUCER_FUNC_DROP_NN, EXP_REDUCER_FUNC_LAST,
-    AlertRulev11, TimeRange, ALERTRULE_STATE_DATA_ALERTING, ALERTRULE_STATE_DATA_OK
+    AlertRulev11, TimeRange, ALERTRULE_STATE_DATA_ALERTING, ALERTRULE_STATE_DATA_OK, ALERTRULE_STATE_DATA_KEEPLAST_V11
 )
 
 from grafanalib.cloudwatch import CloudwatchMetricsTarget
@@ -334,7 +334,7 @@ class ElasticSearchAlertRuleBuilder(AlertRuleBuilder):
                  alert_expression, alert_annotation={},
                  alert_msg="", query="", datasource="", panelId=-1, panel=None, interval_ms=1000, apply_auto_bucket_agg_ids_function=False,
                  metric_aggs=[CountMetricAgg()], time_range=TimeRange('5m', 'now'), time_field="@timestamp",
-                 no_data_alert_state=ALERTRULE_STATE_DATA_OK, execute_error_alert_state=ALERTRULE_STATE_DATA_ALERTING):
+                 no_data_alert_state=ALERTRULE_STATE_DATA_OK, execute_error_alert_state=ALERTRULE_STATE_DATA_KEEPLAST_V11):
         """
         Register a new alert rule.
 
